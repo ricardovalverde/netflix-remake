f58fa12ecdfc55bdc7e9378d114f3e446ce89a4b
ricardovalverde <valverde.ricardojr@gmail.com>
2021-03-23 21:59:37 -0300

    Update JSonDownloadTask.java

--- netflix_remake/app/src/main/java/com/example/netflix/util/JSonDownloadTask.java
+++ netflix_remake/app/src/main/java/com/example/netflix/util/JSonDownloadTask.java
@@ -5,6 +5,11 @@ import android.content.Context;
 import android.os.AsyncTask;
 
 import com.example.netflix.modelo.Categoria;
+import com.example.netflix.modelo.Filme;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -12,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -42,14 +48,20 @@ public class JSonDownloadTask extends AsyncTask<String, Void, List<Categoria>> {
             if (responseCode > 400) {
                 throw new IOException("Erro na conexão com o servidor");
             }
+
+
             InputStream inputStream = urlConnection.getInputStream();
             BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
             String jsonAsString = toString(bufferedInputStream);
+            List<Categoria> categorias = getCategorias(new JSONObject(jsonAsString));
+            bufferedInputStream.close();
 
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -74,6 +86,41 @@ public class JSonDownloadTask extends AsyncTask<String, Void, List<Categoria>> {
         return new String(byteArrayOutputStream.toByteArray());
 
 
+    }
+
+    private List<Categoria> getCategorias(JSONObject jsonObject) throws JSONException {
+        List<Categoria> categorias = new ArrayList<>();
+        JSONArray categoriaArray = jsonObject.getJSONArray("category");
+
+        for (int i = 0; i < categoriaArray.length(); i++) {
+
+            JSONObject jsonObj = categoriaArray.getJSONObject(i);
+
+            String titulo = jsonObj.getString("title");
+
+            List<Filme> filmeList = new ArrayList<>();
+            JSONArray movieArray = jsonObj.getJSONArray("movie");
+
+            Categoria categoria = new Categoria();
+            categoria.setNome(titulo);
+            categoria.setFilmes(filmeList);
+
+
+            for (int j = 0; j < movieArray.length(); j++) {
+                JSONObject filme = movieArray.getJSONObject(j);
+                String cover_url = filme.getString("cover_url");
+                Filme filme1 = new Filme();
+                filme1.setCoverURL(cover_url);
+                filmeList.add(filme1);
+
+
+            }
+
+
+        }
+
+
+        return categorias;
     }
 
 }
